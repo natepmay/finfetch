1dd1e23bad61418aa90c85fb12651591fe2e8aec
Nate May <nathaniel.p.may@gmail.com>
2025-02-05 16:57:03 -0800

    Full modal implementation

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 
-export function Button({
-  disabled,
-  children,
-  onClick,
-}: {
-  disabled: boolean;
+interface Props {
+  disabled?: boolean;
   children: React.ReactNode;
   onClick: () => void;
-}) {
+}
+
+export function Button({ disabled = false, children, onClick }: Props) {
   return (
     <button
       className="bg-blue-700 disabled:bg-gray-500 disabled:cursor-default shadow-lg rounded-full px-5 py-2 text-lg text-white hover:cursor-pointer"
--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -13,19 +13,26 @@ export function Modal({
   const modalRef = useRef<HTMLDialogElement>(null);
 
   useEffect(() => {
-    if (isOpen) {
+    if (isOpen && modalRef.current) {
       modalRef.current?.showModal();
+      document.body.style.overflow = "hidden";
     } else {
       modalRef.current?.close();
     }
+    return () => {
+      document.body.style.overflow = "";
+    };
   }, [isOpen]);
 
   if (!isOpen) return null;
 
   return createPortal(
-    <dialog ref={modalRef} onCancel={onClose}>
+    <dialog
+      ref={modalRef}
+      onCancel={onClose}
+      className="modal fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 backdrop:bg-black/50 p-8 shadow-lg rounded-lg"
+    >
       {children}
-      <button onClick={onClose}>Close</button>
     </dialog>,
     document.body
   );
