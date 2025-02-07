d685cc8ae9c260c6bfadf48653f70af23b8b723f
Nate May <nathaniel.p.may@gmail.com>
2025-02-06 22:32:41 -0800

    Move functions to plaidUtils

--- backend/main.ts
+++ backend/main.ts
@@ -5,14 +5,12 @@ import {
   Configuration,
   PlaidApi,
   PlaidEnvironments,
-  RemovedTransaction,
-  Transaction,
   CountryCode,
   Products,
 } from "npm:plaid";
-import { SimpleTransaction } from "./simpleTransactionObject.ts";
-import { stringify } from "jsr:@std/csv";
 import { DB } from "https://deno.land/x/sqlite/mod.ts";
+import "jsr:@std/dotenv/load";
+
 import {
   initDb,
   getItems,
@@ -23,7 +21,7 @@ import {
   updateAccount,
 } from "./db.ts";
 import { PlaidLinkOnSuccessMetadata } from "./types.ts";
-import "jsr:@std/dotenv/load";
+import { syncTransactions } from "./plaid/plaidUtils.ts";
 
 const app = express();
 const port = 3002;
@@ -62,8 +60,7 @@ const client = new PlaidApi(configuration);
 
 app.get("/api/sync", async (_: express.Request, res: express.Response) => {
   const items = getItems(db);
-  const csvString = await syncTransactions(items);
-  console.log(csvString);
+  const csvString = await syncTransactions(client, items);
   res.attachment("combined.csv").send(csvString);
 });
 
@@ -84,7 +81,6 @@ app.post(
       country_codes: PLAID_COUNTRY_CODES,
       language: "en",
     };
-    console.log(configs);
     try {
       const createTokenResponse = await client.linkTokenCreate(configs);
       res.json(createTokenResponse.data);
@@ -94,6 +90,7 @@ app.post(
   }
 );
 
+// probably should just be "/api/items"
 app.get("/api/getItems", function (_: express.Request, res: express.Response) {
   const items = getItems(db);
   const itemsFrontend = items.map((item) => ({
@@ -104,6 +101,7 @@ app.get("/api/getItems", function (_: express.Request, res: express.Response) {
 });
 
 app.get(
+  // maybe this should be "/api/items/:itemId/accounts"
   "/api/getAccounts",
   function (req: express.Request, res: express.Response) {
     const itemId: string = req.query.itemId;
@@ -156,7 +154,6 @@ app.put(
   ) {
     try {
       const resource = req.body;
-      console.log("resource in main.ts: ", resource);
       const { accountId } = req.params;
       const result = updateAccount(db, accountId, resource);
       res.json({
@@ -191,104 +188,5 @@ app.delete(
 );
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Finfetch listening on port ${port}`);
 });
-
-async function fetchNewSyncData(
-  accessToken: string,
-  initialCursor: string,
-  retriesLeft = 3
-) {
-  const allData = {
-    added: [] as Transaction[],
-    removed: [] as RemovedTransaction[],
-    modified: [] as Transaction[],
-    nextCursor: initialCursor,
-  };
-  if (retriesLeft <= 0) {
-    console.error("Too many retries!");
-    // We're just going to return no data and keep our original cursor. We can try again later.
-    return allData;
-  }
-  try {
-    let keepGoing = false;
-    do {
-      const results = await client.transactionsSync({
-        access_token: accessToken,
-        options: {
-          include_personal_finance_category: true,
-        },
-        cursor: allData.nextCursor,
-      });
-      const newData = results.data;
-      allData.added = allData.added.concat(newData.added);
-      allData.modified = allData.modified.concat(newData.modified);
-      allData.removed = allData.removed.concat(newData.removed);
-      allData.nextCursor = newData.next_cursor;
-      keepGoing = newData.has_more;
-      console.log(
-        `Added: ${newData.added.length} Modified: ${newData.modified.length} Removed: ${newData.removed.length} `
-      );
-
-      // if (Math.random() < 0.5) {
-      //   throw new Error("SIMULATED PLAID SYNC ERROR");
-      // }
-    } while (keepGoing === true);
-    return allData;
-  } catch (error) {
-    // If you want to see if this is a sync mutation error, you can look at
-    // error?.response?.data?.error_code
-    console.log(
-      `Oh no! Error! ${JSON.stringify(
-        error
-      )} Let's try again from the beginning!`
-    );
-    setTimeout(() => {}, 1000);
-    return fetchNewSyncData(accessToken, initialCursor, retriesLeft - 1);
-  }
-}
-
-// TODO consolidate with frontend version
-interface Item {
-  name: string;
-  itemId: string;
-  accessToken: string;
-}
-
-async function syncTransactions(items: Item[]) {
-  const simplifyTransactions = (transactions: Transaction[]) => {
-    return transactions.map((transaction: Transaction) => {
-      return SimpleTransaction.fromPlaidTransaction(transaction);
-    });
-  };
-
-  // TODO get the cursor for each item and store it
-  // TODO currently we add the account id but we want the account name
-
-  const eachItemData = await Promise.all(
-    items.map(async (item) => {
-      const rawData = await fetchNewSyncData(item.accessToken, "");
-      return {
-        added: simplifyTransactions(rawData.added),
-        removed: rawData.removed,
-        modified: simplifyTransactions(rawData.modified),
-      };
-    })
-  );
-
-  const combinedData = eachItemData.reduce((acc, data) => ({
-    added: [...acc.added, ...data.added],
-    removed: [...acc.removed, ...data.removed],
-    modified: [...acc.modified, ...data.modified],
-  }));
-
-  const csvString = stringify(combinedData.added, {
-    columns: Object.keys(combinedData.added[0]),
-  });
-
-  // await Deno.writeTextFile("./out/added.csv", csvString);
-  // TODO support removed (maybe just reverse the transaction?), modified
-  // explore the import in hledger to see what would make sense as default
-
-  return csvString;
-}
---
+++ backend/plaid/plaidUtils.ts
@@ -0,0 +1,108 @@
+import { RemovedTransaction, Transaction, PlaidApi } from "npm:plaid";
+import { SimpleTransaction } from "./simpleTransactionObject.ts";
+import { stringify } from "jsr:@std/csv";
+
+async function fetchNewSyncData(
+  client: PlaidApi,
+  accessToken: string,
+  initialCursor: string,
+  retriesLeft = 3
+) {
+  const allData = {
+    added: [] as Transaction[],
+    removed: [] as RemovedTransaction[],
+    modified: [] as Transaction[],
+    nextCursor: initialCursor,
+  };
+  if (retriesLeft <= 0) {
+    console.error("Too many retries!");
+    // We're just going to return no data and keep our original cursor. We can try again later.
+    return allData;
+  }
+  try {
+    let keepGoing = false;
+    do {
+      const results = await client.transactionsSync({
+        access_token: accessToken,
+        options: {
+          include_personal_finance_category: true,
+        },
+        cursor: allData.nextCursor,
+      });
+      const newData = results.data;
+      allData.added = allData.added.concat(newData.added);
+      allData.modified = allData.modified.concat(newData.modified);
+      allData.removed = allData.removed.concat(newData.removed);
+      allData.nextCursor = newData.next_cursor;
+      keepGoing = newData.has_more;
+      console.log(
+        `Added: ${newData.added.length} Modified: ${newData.modified.length} Removed: ${newData.removed.length} `
+      );
+
+      // if (Math.random() < 0.5) {
+      //   throw new Error("SIMULATED PLAID SYNC ERROR");
+      // }
+    } while (keepGoing === true);
+    return allData;
+  } catch (error) {
+    // If you want to see if this is a sync mutation error, you can look at
+    // error?.response?.data?.error_code
+    console.log(
+      `Oh no! Error! ${JSON.stringify(
+        error
+      )} Let's try again from the beginning!`
+    );
+    setTimeout(() => {}, 1000);
+    return fetchNewSyncData(
+      client,
+      accessToken,
+      initialCursor,
+      retriesLeft - 1
+    );
+  }
+}
+
+// TODO consolidate with frontend version
+interface Item {
+  name: string;
+  itemId: string;
+  accessToken: string;
+}
+
+export async function syncTransactions(client: PlaidApi, items: Item[]) {
+  const simplifyTransactions = (transactions: Transaction[]) => {
+    return transactions.map((transaction: Transaction) => {
+      return SimpleTransaction.fromPlaidTransaction(transaction);
+    });
+  };
+
+  // TODO get the cursor for each item and store it
+  // TODO currently we add the account id but we want the account name
+
+  const eachItemData = await Promise.all(
+    items.map(async (item) => {
+      const rawData = await fetchNewSyncData(client, item.accessToken, "");
+      return {
+        added: simplifyTransactions(rawData.added),
+        removed: rawData.removed,
+        modified: simplifyTransactions(rawData.modified),
+      };
+    })
+  );
+
+  const combinedData = eachItemData.reduce((acc, data) => ({
+    added: [...acc.added, ...data.added],
+    removed: [...acc.removed, ...data.removed],
+    modified: [...acc.modified, ...data.modified],
+  }));
+
+  const csvString = stringify(combinedData.added, {
+    columns: Object.keys(combinedData.added[0]),
+  });
+
+  // await Deno.writeTextFile("./out/added.csv", csvString);
+  // TODO support removed (maybe just reverse the transaction?), modified
+  // explore the import in hledger to see what would make sense as default
+
+  return csvString;
+}
