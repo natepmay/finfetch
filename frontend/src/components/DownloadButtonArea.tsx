2a33915d4071256c279f162ddf5fac6bb1567bd6
Nate May <nathaniel.p.may@gmail.com>
2025-02-13 16:53:46 -0800

    Display error in a modal

--- frontend/src/components/DownloadButtonArea.tsx
+++ frontend/src/components/DownloadButtonArea.tsx
@@ -1,18 +1,24 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import { Button } from "./shared/Button";
+import { Modal } from "./shared/Modal";
 import { downloadWrapper } from "../api";
 import { RefreshContext } from "../context/RefreshContext";
 
+type ModalState = "hidden" | "error";
+
 export function DownloadButtonArea({ disabled }: { disabled: boolean }) {
   const refreshData = useContext(RefreshContext);
+  const [modalState, setModalState] = useState("hidden" as ModalState);
+  const [errorMessage, setErrorMessage] = useState(null as string | null);
 
   async function handleOnClick() {
     try {
       await downloadWrapper();
       refreshData();
     } catch (err) {
-      console.log("We errored! ", err);
+      setErrorMessage((err as Error).message);
+      setModalState("error");
     }
   }
 
@@ -21,6 +27,18 @@ export function DownloadButtonArea({ disabled }: { disabled: boolean }) {
       <Button onClick={handleOnClick} disabled={disabled}>
         Download All
       </Button>
+      <Modal
+        isOpen={modalState === "error"}
+        onClose={() => setModalState("hidden")}
+      >
+        <div className="flex flex-col max-w-md">
+          <h2 className="font-bold text-lg mb-4">Error</h2>
+          <div className="mb-4">{errorMessage}</div>
+          <Button type="submit" onClick={() => setModalState("hidden")}>
+            Okay
+          </Button>
+        </div>
+      </Modal>
     </div>
   );
 }
