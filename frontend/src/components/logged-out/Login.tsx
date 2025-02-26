bde4297c5cf80b56bab94ea7e19430bcafdb626a
Nate May <nathaniel.p.may@gmail.com>
2025-02-26 13:31:58 -0800

    Wipe data on too many retries

--- frontend/src/components/logged-out/Login.tsx
+++ frontend/src/components/logged-out/Login.tsx
@@ -3,13 +3,13 @@ import { useContext, useEffect, useState } from "react";
 import { Button } from "../shared/Button";
 import { Modal } from "../shared/Modal";
 
-import { authenticate, getSalt } from "../../api";
+import { authenticate, getSalt, wipeData } from "../../api";
 
 import { CryptoKeyContext } from "../../context/CryptoKeyContext";
 
 export function Login() {
   const [modalState, setModalState] = useState(
-    "hidden" as "hidden" | "wrongPw"
+    "hidden" as "hidden" | "wrongPw" | "wiped"
   );
   const [userStatus, setUserStatus] = useState(
     "loading" as "loading" | "existent" | "nonexistent"
@@ -28,6 +28,12 @@ export function Login() {
     if (result) {
       setCryptoKey(result);
     } else {
+      if (retries === 1) {
+        wipeData();
+        setModalState("wiped");
+        // don't need to reset retries because they'll need to reload the page to get back to it
+        return;
+      }
       setRetries((previous) => previous - 1);
       setModalState("wrongPw");
     }
@@ -47,7 +53,7 @@ export function Login() {
     return () => {
       active = false;
     };
-  });
+  }, [modalState]);
 
   const userExists = (
     <form onSubmit={handleEnterPw} className="flex flex-col">
@@ -85,6 +91,18 @@ export function Login() {
         <h2 className="font-bold text-lg mb-4">Incorrect Password</h2>
         <h3 className="mb-4">
           Try again or start from scratch. Tries remaining: {retries}.
+        </h3>
+        <Button onClick={() => setModalState("hidden")}>Okay</Button>
+      </Modal>
+
+      <Modal
+        isOpen={modalState === "wiped"}
+        onClose={() => setModalState("hidden")}
+      >
+        <h2 className="font-bold text-lg mb-4">Too Many Retries</h2>
+        <h3 className="mb-4">
+          You've entered the wrong password too many times. Your data has been
+          reset, and you may now start from scratch.
         </h3>
         <Button onClick={() => setModalState("hidden")}>Okay</Button>
       </Modal>
